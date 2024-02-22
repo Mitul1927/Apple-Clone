397ac8f8007629bb638c3eb111dee814d7b849ed
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2024-02-22 21:31:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* ************************************************************************ */
-// section 3 video pause button 
+// section 3 video pause button
 var video = document.getElementById("myVideo");
 // Get the button
 var btn = document.getElementById("myBtn");
+var icon = document.getElementById("icon");
 // Pause and play the video, and change the button text
 function myFunction() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Play";
-    }
+  if (video.paused) {
+    video.play();
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+    //btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+    //btn.innerHTML = "Play";
+  }
 }
 /* ************************************************************************ */
-
